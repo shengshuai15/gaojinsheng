28a526aafca4ba46d1788f5776e64af18ec6cdf2
username <gjinsheng@icloud.com>
2023-11-08 15:23:37 +0800

    11月8号，问他分布式电源减容，啥也不会

--- src/Test01/demo.java
+++ src/Test01/demo.java
@@ -4,9 +4,6 @@ import java.io.File;
 
 public class demo {
     public static void main(String[] args) {
-        String dir = "C:\\Users\\gjins\\Desktop\\";
-        String b = "文本文档.txt";
-        File a = new File(dir,b);
-        System.out.println(a.getPath());
+        System.out.println("卢午龙大沙坝");
     }
 }
